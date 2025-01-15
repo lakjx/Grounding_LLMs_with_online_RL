2fac7fd9ffc052ab8b25b1b6b37582c8510c6987
lakjx <1156097487@qq.com>
2025-01-15 11:02:06 +0800

    laege continued

--- train.sh
+++ train.sh
@@ -7,7 +7,7 @@ export RANK=1
 export LOCAL_RANK=1
 export MASTER_ADDR=127.0.0.1
 export MASTER_PORT=370
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 export NCCL_P2P_DISABLE=1
 export NCCL_IB_DISABLE=1
 
@@ -22,9 +22,9 @@ UE_NUM=5
 SEED=1113
 WANDB_INIT=true
 WANDB_PROJECT="LLM_PPO_MacEnv"
-WANDB_NAME="UE_${UE_NUM}_large"
+WANDB_NAME="UE_${UE_NUM}_large_continued"
 
-# LOAD_PATH="/home/trx/workplace/trx/output_ue5_t5_base/epochs_50-100"
+LOAD_PATH="/home/trx/workplace/trx/output_ue5_t5_large/continued_epochs_500-600"
 # 执行 Python 脚本
 python -m lamorel_launcher.launch \
     --config-path "$CONFIG_PATH" \
@@ -38,5 +38,5 @@ python -m lamorel_launcher.launch \
     "macEnv_args.UE_num=$UE_NUM" \
     "macEnv_test_args.UE_num=$UE_NUM" \
     "macEnv_test_args.seed=$SEED" \
-    # "rl_script_args.loading_path=$LOAD_PATH" \
+    "rl_script_args.loading_path=$LOAD_PATH" \
 
